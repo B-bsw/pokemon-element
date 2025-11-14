3221f01813ab3f8e0b8bbe93d598f76cefbe8dfb
BB <sorawitthassadorn15@gmail.com>
2025-11-14 23:06:47 +0700

    change: route table

---
+++ src/app/pokemon/type/page.tsx
@@ -0,0 +1,11 @@
+import TablePokemon from '@/components/table/TablePokemon'
+
+const Page = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen p-4">
+            <TablePokemon />
+        </div>
+    )
+}
+
+export default Page
